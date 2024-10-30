fa948707909cfa2bf303c53f10ea03636d397b62
Olof Bjarnason <olof.bjarnason@gmail.com>
2024-10-30 21:33:36 +0100

    Add biomejs for convenient autoformatting

--- src/assets/simulation.test.ts
+++ src/assets/simulation.test.ts
@@ -1,7 +1,19 @@
-import {describe, expect, it} from "vitest";
+import { describe, expect, it } from "vitest";
+
+function levelForXP(totalXP: number) {
+	if (totalXP < 10) return 0;
+	return 1;
+}
 
 describe("simulation", () => {
-  it("...", () => {
-    expect(1).toStrictEqual(1)
-  })
-})
+	/* levelling up from 0 requires 10 xp
+      - " -  1 requires 20 xp
+      - " -  2   "      20+10 = 30 xp
+      - " -  3   "      30 + 20 = 50 xp
+   */
+	it("levels and XP", () => {
+		expect(levelForXP(0)).toStrictEqual(0);
+		expect(levelForXP(9)).toStrictEqual(0);
+		expect(levelForXP(10)).toStrictEqual(1);
+	});
+});
