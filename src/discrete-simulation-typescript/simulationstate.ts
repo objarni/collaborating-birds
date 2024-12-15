2a729070dcdba107c94ecff240416621185a66ba
Olof Bjarnason <olof.bjarnason@gmail.com>
2024-12-15 11:00:31 +0100

    move simulationstate to discrite-simulation-typescript

---
+++ src/discrete-simulation-typescript/simulationstate.ts
@@ -0,0 +1,7 @@
+import {PriorityQueue} from "priority-queue-typescript";
+
+export interface SimulationState<EventType extends object, SystemStateType> {
+	eventQueue: PriorityQueue<EventType>;
+	systemState: SystemStateType;
+	time: number;
+}
