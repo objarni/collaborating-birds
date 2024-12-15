d69d80778caf30ec4a1a29a0967eca91c7178ce5
Olof Bjarnason <olof.bjarnason@gmail.com>
2024-12-15 11:01:23 +0100

    rename

--- src/barbershop.types.ts
+++ src/barbershop.types.ts
@@ -1,4 +1,4 @@
-import type { SimEvent } from "./discrete-simulation-typescript/simevent.ts";
+import type { SimEvent } from "./discrete-event-simulation-typescript/simevent.ts";
 
 export interface BarberShopState {
 	missedClients: number;
