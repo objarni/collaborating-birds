0d371b2cb07630a216ddcf52f67ba712f2fa750b
Olof Bjarnason <olof.bjarnason@gmail.com>
2024-12-27 23:50:11 +0100

    Tidy: naming

--- src/discrete-event-simulation-typescript/simulationstate.ts
+++ src/discrete-event-simulation-typescript/simulationstate.ts
@@ -1,8 +1,8 @@
 import type { PriorityQueue } from "priority-queue-typescript";
 import type {SimEvent} from "./simevent.ts";
 
-export interface SimulationState<EventType extends object, SystemStateType> {
+export interface SimulationState<EventType extends object, StateType> {
 	eventQueue: PriorityQueue<SimEvent<EventType>>;
-	systemState: SystemStateType;
+	systemState: StateType;
 	time: number;
 }
