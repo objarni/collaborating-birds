4b588d1fc028da66d7fbb96c26155d54b81907a4
Olof Bjarnason <olof.bjarnason@gmail.com>
2024-12-28 09:34:07 +0100

    Rename

--- src/barbershop.ts
+++ src/barbershop.ts
@@ -9,7 +9,7 @@ import type {
 	ChairStates,
 	Customer,
 } from "./barbershop.types.ts";
-import { simStep } from "./discrete-event-simulation-typescript/simstep.ts";
+import { simStep } from "./discrete-event-simulation-typescript/sim.ts";
 
 export function barberShopEventHandler(
 	systemState: BarberShopState,
